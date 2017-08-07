e6b7287e59e23a7e35c323534c367fdfe9455fc1
camschoch <camschoch3@gmail.com>
2017-08-07 07:17:45 -0500

    first commit

---
+++ ClockRadio/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clockRadio runProgram = new clockRadio();
+            Console.WriteLine("type one of these to perform the task: time, alarm on, alarm off, set alarm, radio on, radio off, set radio.");
+            string userInput = Console.ReadLine();
+
+            switch(userInput)
+            {
+                case "time":
+                    runProgram.getTime();
+                    break;
+                case "alarm on":
+                    runProgram.setAlarmOn();
+                    break;
+                case "alarm off":
+                    runProgram.setAlarmOff();
+                    break;
+                case "set alarm":
+                    runProgram.setAlarmTime();
+                    break;
+                case "radio on":
+                    runProgram.turnOnRadio();
+                    break;
+                case "radio off":
+                    runProgram.turnOffRadio();
+                    break;
+                case "set radio":
+                    runProgram.setRadioStation();
+                    break;
+                default:
+                    Console.WriteLine("Sorry that did not work");
+                    break;
+            }
+        }
+    }
+}
---
+++ ClockRadio/clockRadio.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class clockRadio
+    {
+        bool alarmStatus;
+        public void getTime()
+        {
+            Console.WriteLine("07:30");
+            Console.ReadLine();
+        }
+        public void setAlarmOn()
+        {
+            Console.WriteLine("Alarm is on");
+            Console.ReadLine();
+        }
+        public void setAlarmOff()
+        {
+            Console.WriteLine("Alarm is off");
+            Console.ReadLine();
+        }
+
+        public void setAlarmTime()
+        {
+            Console.WriteLine("enter the time you would like to set the alarm to.");
+            string alarmTime = Console.ReadLine();
+            Console.WriteLine("The alarm is set to " + alarmTime);
+            Console.ReadLine();
+        }
+        public void setRadioStation()
+        {
+            Console.WriteLine("enter the radio station you would like to set the radio to.");
+            string radioStation = Console.ReadLine();
+            Console.WriteLine("The radio station is set to " + radioStation);
+            Console.ReadLine();
+        }
+        public void turnOnRadio()
+        {
+            Console.WriteLine("Radio is on");
+            Console.ReadLine();
+        }
+        public void turnOffRadio()
+        {
+            Console.WriteLine("Radio is off");
+            Console.ReadLine();
+        }
+
+
+    }
+}
